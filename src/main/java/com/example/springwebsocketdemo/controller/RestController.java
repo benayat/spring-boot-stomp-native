0909b1d279fec601dcfb75bc2e1949ec5c86b098
Benaya Trabelsi <benaya7@gmail.com>
2023-04-28 18:32:51 +0300

    controller for testing

---
+++ src/main/java/com/example/springwebsocketdemo/controller/RestController.java
@@ -0,0 +1,13 @@
+package com.example.springwebsocketdemo.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RequiredArgsConstructor
+@org.springframework.web.bind.annotation.RestController
+public class RestController {
+    @GetMapping
+    public String hello(){
+        return "hello world";
+    }
+}

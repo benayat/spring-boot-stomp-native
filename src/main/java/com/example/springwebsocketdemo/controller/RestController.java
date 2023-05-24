5503db45fbd17422fee460e4bf4b38b275e433f8
Benaya Trabelsi <benaya7@gmail.com>
2023-05-24 14:23:14 +0300

    added prometheus and actuator for monitoring

--- src/main/java/com/example/springwebsocketdemo/controller/RestController.java
+++ src/main/java/com/example/springwebsocketdemo/controller/RestController.java
@@ -1,13 +1,16 @@
 package com.example.springwebsocketdemo.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RequiredArgsConstructor
+@Slf4j
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
     @GetMapping("/hello")
     public String hello(){
+        log.info("got request from client!");
         return "hello world";
     }
 }
